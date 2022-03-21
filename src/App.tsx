dd855a5f22f4a6e3984162e337c7ad2fb4465dfa
Drücke, David (SE-A/33) <david.druecke@volkswagen.de>
2022-03-21 16:25:18 +0100

    add searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/header/header'
+import Searchbar from './components/searchbar/searchbar';
 
 function App() {
   return (
     <div className="App">
-    
+      <Header/>
+      <Searchbar/>
     </div>
   );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import "./header.css"
+import logo from "./../../res/img/logo.jpeg";
 
-const header = () => {
-  return (
-    <div>header</div>
-  )
-}
+const Header = () => {
 
-export default header
+    const [hamburgerOpen, setHamburgerOben] = useState(false);
+    const [headerBg, setHeaderBg] = useState({
+      backgroundColor: 'rgba(31, 30, 30, 0.00)'
+    });
+  
+    const hamburgerHandler = () => {
+      setHamburgerOben(!hamburgerOpen);
+    };
+  
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        var scroll_position = window.scrollY;
+        if (scroll_position > 250) {
+          setHeaderBg({ backgroundColor: "#3acbf7" });
+        } else {
+          setHeaderBg({ backgroundColor: "transparent" });
+        }
+      });
+    });
+  
+    const menuItems = [
+      { href: "#signup", dataAfter: "Become a host",className: "host"},
+      { href: "#signup", dataAfter: "sign up",className: "signup"},
+      { href: "#login", dataAfter: "login",className: "login"},
+    ];
+  
+    return (
+      <section id="header" style={headerBg}>
+        <div className="header container">
+          <div className="nav-bar">
+            <div className="brand">
+              <a href="#hero">
+                  <img src={logo} alt="Logo" style={{width:30,height:30}}/>
+                <h1>
+                  <span>C</span>ar<span>D</span>ealer
+                </h1>
+              </a>
+            </div>
+            <div className="nav-list">
+              <div
+                className={hamburgerOpen ? "hamburger active" : "hamburger"}
+                onClick={hamburgerHandler}
+              >
+                <div className="bar"></div>
+              </div>
+              <ul className={hamburgerOpen ? "active" : undefined}>
+                {menuItems.map((item, index) => (
+                  <li key={index}>
+                    <a
+                      href={item.href}
+                      data-after={item.dataAfter}
+                      className={item.className}
+                      onClick={hamburgerHandler}
+                    >
+                      {item.dataAfter}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Header;
