b0cd9ba75b685dfb7058bf1c190e87a165a74da7
Drücke, David (SE-A/33) <david.druecke@volkswagen.de>
2022-03-22 13:16:36 +0100

    add react-datepicker

---
+++ src/components/body/hero/searchbar/searchbar.js
@@ -0,0 +1,46 @@
+import React,{useState} from "react";
+import "./searchbar.css"
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+
+const Searchbar = () => {
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  return (
+    <div id="searchbar">
+      <div className="destination">
+        <p>Where</p>
+        <input type="text" placeholder="City..." />
+      </div>
+      <div className="from">
+        <p>From</p>
+        <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+      />
+      </div>
+      <div className="until">
+        <p>Until</p>
+        <DatePicker
+        selected={endDate}
+        onChange={(date) => setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate}
+      />
+      </div>
+      <div className="submit-div">
+      <button type="button">Search..</button>
+      </div>
+    </div>
+  );
+};
+
+export default Searchbar;
