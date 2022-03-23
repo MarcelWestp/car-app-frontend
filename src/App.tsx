43c2aaed43ea94035c2dd2e29ff5761ae8b189ab
Drücke, David (SE-A/33) <david.druecke@volkswagen.de>
2022-03-23 10:39:10 +0100

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import './App.css';
-import Header from './components/header/header'
-import Footer from './components/footer/footer'
-import Landingpage from './pages/landingpage'
-import Signup from './pages/signup'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Landingpage from "./pages/landingpage";
+import Signup from "./pages/signup";
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"  element={ <Landingpage />} />
+          <Route path="signup" element={ <Signup />} />
+        </Routes>
+      </BrowserRouter>
+      ,
     </div>
   );
 }
