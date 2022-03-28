1e5eb8a09701d7a76186f70d8bf43750c333efcc
Drücke, David (SE-A/33) <david.druecke@volkswagen.de>
2022-03-28 12:07:32 +0100

    add profilemodal

---
+++ src/components/profil/profiledetail/editmodal.tsx
@@ -0,0 +1,124 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import User from "./../../../models/User";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+
+const Editmodal = ({ user }: { user: User }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [value, setValue] = React.useState<Date | null>(user.dob);
+
+  return (
+    <div>
+      <Button variant="contained" onClick={handleClickOpen}>
+        Edit Profile
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Edit Profile:</DialogTitle>
+        <DialogContent>
+          <Box
+            sx={{
+              display: "grid",
+              gap: 1,
+              gridTemplateColumns: "repeat(2, 1fr)",
+              marginTop: 2,
+            }}
+          >
+            <p>Username: </p>
+            <TextField
+              required
+              id="outlined-required"
+              label="Username:"
+              defaultValue={user.username}
+            />
+            <p>Firstname:</p>
+            <TextField
+              required
+              id="outlined-required"
+              label="Firstname:"
+              defaultValue={user.firstName}
+            />
+            <p>Lastname:</p>
+            <TextField
+              required
+              id="outlined-required"
+              label="Lastname:"
+              defaultValue={user.lastName}
+            />
+            <p>Email:</p>
+            <TextField
+              required
+              id="outlined-required"
+              label="Email:"
+              defaultValue={user.email}
+            />
+            <p>Birthdate:</p>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DatePicker
+                label="Birthdate"
+                value={value}
+                onChange={(newValue) => {
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            <p>Adress:</p>
+            <p></p>
+            <p>Steet:</p>
+            <TextField
+              required
+              id="outlined-required"
+              label="Street:"
+              defaultValue={user.address.street}
+            />
+            <p>Nr:</p>
+            <TextField
+              required
+              id="outlined-required"
+              label="Street:"
+              defaultValue={user.address.nr}
+            />
+            <p>City:</p>
+            <TextField
+              required
+              id="outlined-required"
+              label="City:"
+              defaultValue={user.address.city}
+            />
+            <p>Zip:</p>
+            <TextField
+              required
+              type="number"
+              id="outlined-required"
+              label="Zip:"
+              defaultValue={user.address.zip}
+            />
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Edit</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Editmodal;
--- src/components/profil/profiledetail/profiledetail.tsx
+++ src/components/profil/profiledetail/profiledetail.tsx
@@ -1,39 +1,55 @@
-import React from 'react'
-import Box from '@mui/material/Box'
-import Avatar from '@mui/material/Avatar';
-import User from './../../../models/User'
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import User from "./../../../models/User";
+import Button from "@mui/material/Button";
+import Editmodal from "./editmodal";
 
-const portrait: string = "https://cdn.pixabay.com/photo/2016/03/27/07/26/darth-vader-1282288_1280.jpg"
+const portrait: string =
+  "https://cdn.pixabay.com/photo/2016/03/27/07/26/darth-vader-1282288_1280.jpg";
 
 const Profiledetail = ({ user }: { user: User }) => {
-    return (
-        <div className="profiledetail">
-            <Box sx={{ marginTop: 2, marginLeft: 5}}>
-                <Avatar alt="Mustermann" src={portrait} sx={{ width: 128, height: 128 }} />
-                <Box sx={{
-                    display: 'grid',
-                    gap: 1,
-                    gridTemplateColumns: 'repeat(2, 1fr)',
-                    marginTop: 2
-                }}>
-                    <p>Username: </p>
-                    <p>{user.username}</p>
-                    <p>Firstname:</p>
-                    <p>{user.firstName}</p>
-                    <p>Lastname:</p>
-                    <p>{user.lastName}</p>
-                    <p>Email:</p>
-                    <p>{user.email}</p>
-                    <p>Birthdate:</p>
-                    <p>{user.dob.toLocaleString().substring(0, 10)}</p>
-                    <p>Adress:</p>
-                    <p>{user.address.street} {user.address.nr}</p>
-                    <p>City:</p>
-                    <p>{user.address.zip} {user.address.city}</p>
-                </Box>
-            </Box>
-        </div>
-    )
-}
+  return (
+    <div className="profiledetail">
+      <Box sx={{ marginTop: 2, marginLeft: 5 }}>
+        <Avatar
+          alt="Mustermann"
+          src={portrait}
+          sx={{ width: 128, height: 128 }}
+        />
+        <Box
+          sx={{
+            display: "grid",
+            gap: 1,
+            gridTemplateColumns: "repeat(2, 1fr)",
+            marginTop: 2,
+          }}
+        >
+          <p>Username: </p>
+          <p>{user.username}</p>
+          <p>Firstname:</p>
+          <p>{user.firstName}</p>
+          <p>Lastname:</p>
+          <p>{user.lastName}</p>
+          <p>Email:</p>
+          <p>{user.email}</p>
+          <p>Birthdate:</p>
+          <p>{user.dob.toLocaleString().substring(0, 10)}</p>
+          <p>Adress:</p>
+          <p>
+            {user.address.street} {user.address.nr}
+          </p>
+          <p>City:</p>
+          <p>
+            {user.address.zip} {user.address.city}
+          </p>
+          <Box sx={{ gridColumn: 2 }}>
+            <Editmodal user={user} />
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  );
+};
 
-export default Profiledetail
+export default Profiledetail;
