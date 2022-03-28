fe4c273eaa8b687414150248a0f2f4ec0390a533
Drücke, David (SE-A/33) <david.druecke@volkswagen.de>
2022-03-28 09:37:03 +0100

    new searchbar

--- src/components/landingpage/hero/hero.tsx
+++ src/components/landingpage/hero/hero.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Searchbar from "./searchbar/searchbar"
+import NewSearchbar from './searchbar/searchbarNew'
 import "./hero.css"
 
 const Hero = () => {
   return (
     <div id="hero">
-        <Searchbar/>
+        {/* <Searchbar/> */}
+        <NewSearchbar />
         <div className="title">
           <h1>Search for a new Adventure?</h1>
           <h2>Find the right car here!...</h2>
