531b1454f7a51206ba674c48bca96d2c2ee9e65b
Drücke, David (SE-A/33) <david.druecke@volkswagen.de>
2022-03-29 15:40:22 +0100

    finish usestate hostacar

--- src/pages/hostacar.tsx
+++ src/pages/hostacar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import Box from "@mui/material/Box";
@@ -24,7 +24,6 @@ import sport from "./../res/img/cartypes/sport.jpeg";
 import suv from "./../res/img/cartypes/suv.jpeg";
 
 const handleDragStart = (e: any) => e.preventDefault();
-
 const items = [
   <div>
     <img
@@ -74,11 +73,6 @@ const Input = styled("input")({
 });
 
 const Hostacar = () => {
-  const [make, setMake] = React.useState("");
-  const handleChangeMake = (event: SelectChangeEvent) => {
-    setMake(event.target.value as string);
-  };
-
   const featuresArray = [
     "4x4",
     "Backup Camera",
@@ -92,7 +86,7 @@ const Hostacar = () => {
   ];
   const guidelinesArray = ["no smoking", "no pets"];
 
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -102,8 +96,38 @@ const Hostacar = () => {
     setOpen(false);
   };
 
+  const [make, setMake] = useState("");
+  const [model, setModel] = useState("");
+  const [type, setType] = useState("");
+  const [year, setYear] = useState("");
+  const [fuel, setFuel] = useState("");
+  const [seats, setSeats] = useState("");
+  const [pricePerDay, setPricePerDay] = useState("");
+  const [distancePerDay, setDistancePerDay] = useState("");
+  const [street, setStreet] = useState("");
+  const [number, setNumber] = useState("");
+  const [city, setCity] = useState("");
+  const [zip, setZip] = useState("");
+
+  const handleChangeHandler = (
+    event: SelectChangeEvent | React.ChangeEvent<HTMLInputElement>,
+    setter: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    setter(event.target.value as string);
+  };
+
+  const [features,setFeatures] = useState<string[]>([]);
+  const [guidelines,setGuidelines] = useState<string[]>([]);
+
+  const multihandler = (value: string[],setter:React.Dispatch<React.SetStateAction<string[]>>) =>{
+    setter(value);
+  }
+
+
+ 
+
   return (
-    <Box style={{ margin: "20px 20%" }}>
+    <Box style={{ margin: "20px 20%", maxWidth: 800 }}>
       <h2>List your car</h2>
       <Box style={{ margin: "10px 0px 20px 0" }}>
         <AliceCarousel
@@ -135,45 +159,84 @@ const Hostacar = () => {
           marginBottom: 2,
         }}
       >
-        <h2 >Car:</h2><p></p>
+        <h2>Car:</h2>
+        <p></p>
         <p>Make:</p>
-        <TextField id="make" label="Make" variant="standard" />
+        <TextField
+          id="make"
+          label="Make"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setMake)
+          }
+        />
         <p>Model:</p>
-        <TextField id="model" label="Model" variant="standard" />
+        <TextField
+          id="model"
+          label="Model"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setModel)
+          }
+        />
         <p>Type:</p>
         <Select
           labelId="type"
           id="type"
-          value={make}
-          onChange={handleChangeMake}
+          value={type}
+          onChange={(event: SelectChangeEvent) =>
+            handleChangeHandler(event, setType)
+          }
         >
-          <MenuItem value={10}>Classic</MenuItem>
-          <MenuItem value={20}>Luxury</MenuItem>
-          <MenuItem value={30}>Sport</MenuItem>
-          <MenuItem value={40}>SUV</MenuItem>
+          <MenuItem value={"Classic"}>Classic</MenuItem>
+          <MenuItem value={"Luxury"}>Luxury</MenuItem>
+          <MenuItem value={"Sport"}>Sport</MenuItem>
+          <MenuItem value={"SUV"}>SUV</MenuItem>
         </Select>
         <p>Year:</p>
-        <TextField id="year" type="number" label="year" variant="standard" />
+        <TextField
+          id="year"
+          type="number"
+          label="year"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setYear)
+          }
+        />
         <p>Fuel:</p>
-        <TextField id="fuel" label="Fuel" variant="standard" />
+        <Select
+          labelId="fuel"
+          id="fuel"
+          value={fuel}
+          onChange={(event: SelectChangeEvent) =>
+            handleChangeHandler(event, setFuel)
+          }
+        >
+          <MenuItem value={"Petrol"}>Petrol</MenuItem>
+          <MenuItem value={"Diesel"}>Diesel</MenuItem>
+          <MenuItem value={"Hybrid"}>Hybrid</MenuItem>
+          <MenuItem value={"Electric"}>Electric</MenuItem>
+        </Select>
         <p>Seats:</p>
-        <TextField id="seats" type="number" label="seats" variant="standard" />
-        <p>Seats:</p>
-        <TextField id="seats" type="number" label="seats" variant="standard" />
-        <h2 >Pricing:</h2><p></p>
-        <p>Price per day in $:</p>
-        <TextField id="zip" type="number" label="price per day in dollors" variant="standard" />
-        <p>Distance per day in miles:</p>
-        <TextField id="zip" type="number" label="distance per day in miles" variant="standard" />
+        <TextField
+          id="seats"
+          type="number"
+          label="seats"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setSeats)
+          }
+        />
       </Box>
       <Grid container spacing={2}>
         <Grid item xs={4}>
           <p>Fetures:</p>
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={8} style={{ marginTop: 15 }}>
           <Autocomplete
             multiple
             id="tags-filled"
+            onChange={(event:any,value:string[]) => multihandler(value,setFeatures)}
             options={featuresArray.map((feature) => feature)}
             freeSolo
             renderTags={(value: readonly string[], getTagProps) =>
@@ -186,7 +249,11 @@ const Hostacar = () => {
               ))
             }
             renderInput={(params) => (
-              <TextField {...params} variant="filled" placeholder="Features" />
+              <TextField
+              {...params}
+              variant="standard"
+              placeholder="Features"
+              />
             )}
           />
         </Grid>
@@ -213,9 +280,10 @@ const Hostacar = () => {
         <Grid item xs={4}>
           <p>Guidlines:</p>
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={8} style={{ marginTop: 15 }}>
           <Autocomplete
             multiple
+            onChange={(event:any,value:string[]) => multihandler(value,setGuidelines)}
             id="tags-filled"
             options={guidelinesArray.map((guideline) => guideline)}
             freeSolo
@@ -229,7 +297,11 @@ const Hostacar = () => {
               ))
             }
             renderInput={(params) => (
-              <TextField {...params} variant="filled" placeholder="Guideline" />
+              <TextField
+                {...params}
+                variant="standard"
+                placeholder="Guideline"
+              />
             )}
           />
         </Grid>
@@ -243,15 +315,71 @@ const Hostacar = () => {
           marginBottom: 2,
         }}
       >
-        <h2>Car location:</h2><p></p>
+        <h2>Pricing:</h2>
+        <p></p>
+        <p>Price per day in $:</p>
+        <TextField
+          id="priceperday"
+          type="number"
+          label="price per day in dollors"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setPricePerDay)
+          }
+        />
+        <p>Distance per day in miles:</p>
+        <TextField
+          id="zip"
+          type="number"
+          label="distance per day in miles"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setDistancePerDay)
+          }
+        />
+        <h2>Car location:</h2>
+        <p></p>
         <p>Street:</p>
-        <TextField id="street" label="Street" variant="standard" />
+        <TextField
+          id="street"
+          label="Street"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setStreet)
+          }
+        />
         <p>Nr:</p>
-        <TextField id="nr" label="Number" variant="standard" />
+        <TextField
+          id="nr"
+          label="Number"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setNumber)
+          }
+        />
         <p>City:</p>
-        <TextField id="city" label="City" variant="standard" />
+        <TextField
+          id="city"
+          label="City"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setCity)
+          }
+        />
         <p>Zip:</p>
-        <TextField id="zip" type="number" label="Zip" variant="standard" />
+        <TextField
+          id="zip"
+          type="number"
+          label="Zip"
+          variant="standard"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeHandler(event, setZip)
+          }
+        />
+        <p></p>
+        <Button style={{ marginTop: 20, height: 40 }} variant="contained">
+          <h2>Continue</h2>
+        </Button>
       </Box>
     </Box>
   );
